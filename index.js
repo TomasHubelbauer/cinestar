df0db843bc7e9b71a56ed7cc69ea3e67d86e8de0
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-10 17:55:45 +0200

    Return instead of emailing self

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
 const path = require('path');
-const email = require('../self-email');
-const { eml, subject, sender, recipient } = require('../self-email');
+
+/** @typedef {{ name: string; posterUrl: string; date: Date; }} Title */
 
 module.exports = async function () {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const [page] = await browser.pages();
   await page.goto('https://www.cinestar.cz/cz/praha9/program');
-
-  /** @typedef {{ name: string; posterUrl: string; date: Date; }} Title */
 
   const titlesJsonFilePath = path.join(__dirname, 'titles.json');
 
@@ -25,6 +23,8 @@ module.exports = async function () {
   catch (error) {
     // Ignore missing or broken `titles.json`
   }
+
+  const messages = [];
 
   const typeCodes = ['atmos', 'tag-2d', 'tag-3d'];
   const typeNames = ['Unknown', 'Atmos', '2D', '3D'];
@@ -48,7 +48,7 @@ module.exports = async function () {
     for (const roomTable of await page.$$(`#ctab${offset} #tableProgram`)) {
       const typeCode = await roomTable.evaluate(roomTable => roomTable.className);
       const typeName = typeNames[typeCodes.indexOf(typeCode) + 1 /* Shift to make `-1` into `0` for catch-all */];
-      console.log('\t', typeName);
+      console.log(day, typeName);
 
       for (const titleTd of await roomTable.$$('.tdTitle')) {
         const name = await titleTd.$eval('.title a', a => a.textContent);
@@ -56,19 +56,12 @@ module.exports = async function () {
 
         const exists = !!titles.find(title => title.name === name);
         if (!exists) {
-          console.log('\t\t', name, '[NEW]');
+          console.log(day, typeName, name, '[NEW]');
           titles.push({ name, posterUrl, date });
-          await email(
-            eml(
-              subject(name),
-              sender('Cinestar <bot+cinestar@hubelbauer.net>'),
-              recipient('Tomas Hubelbauer <tomas@hubelbauer.net>'),
-              `<p>${name} premieres at ${date.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long' })}!</p>`,
-            )
-          );
+          messages.push(`${name} premieres at ${date.toLocaleDateString('en-US', { weekday: 'long', day: 'numeric', month: 'long' })}!`);
         }
         else {
-          console.log('\t\t', name);
+          console.log(day, typeName, name, '[KNOWN]');
         }
       }
     }
@@ -87,26 +80,19 @@ module.exports = async function () {
     }
   }
 
-  await email(
-    eml(
-      subject(tonight.map(t => t.name).join(', ')),
-      sender('Cinestar <bot+cinestar@hubelbauer.net>'),
-      recipient('Tomas Hubelbauer <tomas@hubelbauer.net>'),
-      ...(tonight.length === 0
-        ? [
-          `<p>CineStar doesn't screen any titles tonight.</p>`
-        ]
-        : [
-          `<p>CineStar screens ${tonight.length} titles tonight!</p>`,
-          ...tonight.map(t => `<p>${t.name}</p>\n<img src="${t.posterUrl}" />`),
-        ]
-      ),
-    )
-  );
+  if (tonight.length === 0) {
+    messages.push(`CineStar doesn't screen any titles tonight.`);
+  }
+  else {
+    messages.push(`CineStar screens ${tonight.length} titles tonight!`);
+    for (const title of tonight) {
+      messages.push(`${t.name} <img src="${t.posterUrl}" />`);
+    }
+  }
 
-  return `${tonight.length} titles tonight`;
+  return messages;
 };
 
 if (process.cwd() === __dirname) {
-  module.exports();
+  module.exports().then(console.log);
 }
